5c8ac943b4374e8acfd0ffe4872aa71e889da8c5
Atanas <atanasormanow@gmail.com>
2021-04-01 11:52:32 +0300

    Use sudo for ln

--- distribute_dots.sh
+++ distribute_dots.sh
@@ -3,7 +3,7 @@
 sym_target (){
     DEST=$(envsubst < $1/dest)
     FILE_NAME=$(echo $DEST | rev | cut -d '/' -f 1 | rev)
-    ln -si $(pwd)/$1/$FILE_NAME $DEST
+    sudo ln -si $(pwd)/$1/$FILE_NAME $DEST
 }
 
 for fold in $(ls -d dotfiles/*); do
